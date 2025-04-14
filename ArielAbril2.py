def6aef44cf903aa606d3ef5cd0d1efe7593492a
Dizs-sziD <avruizdiaz@escuelasproa.edu.ar>
2025-04-14 12:07:18 -0300

    Add files via upload

---
+++ ArielAbril2.py
@@ -0,0 +1,23 @@
+cipote = 2
+while cipote == 2:
+    
+    print("Estas en el gestionador de tareas proa")
+    print("¿Que quieres hacer?")
+    print("1 = Añadir tareas")
+    print("2 = ver tareas")
+    print("3 = marcar una tarea como completada")
+    print("4 = salir")
+    que_hacer = int(input("~"))
+    
+    
+
+    if que_hacer == 1:
+        print("añadir tareas")
+    elif que_hacer == 2:
+        print("ver tareas ")
+    elif que_hacer == 3:
+        print("marcar tarea por completada")
+
+    elif que_hacer == 4 :
+        cipote = 1 
+    
